f153d0ab209107a575292acfc4f5796c34e43722
Amal Chawla <chawla.amal.170401@gmail.com>
2021-10-12 22:54:35 +0530

    offset removed teamcard

--- src/Components/teamCard.js
+++ src/Components/teamCard.js
@@ -12,9 +12,8 @@ function TeamCard() {
         <div>
             <section className="main-content">
                 <div className="container">
-			
-                <div className="row scale-1">
-                    <div className="col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+                <div className="row">
+                    <div className="col-sm-8 col-md-6 col-lg-4">
                         <div className="profile-card card rounded-lg shadow p-4 p-xl-5 mb-4 text-center position-relative overflow-hidden">
                             <div className="banner"></div>
                             <img src={user1} alt="" className="img-circle mx-auto mb-3"/>
@@ -33,9 +32,68 @@ function TeamCard() {
                             </div>
                         </div>
                     </div>
-                </div>
 
-		    
+                    <div className="col-sm-8 col-md-6 col-lg-4 ">
+                        <div className="profile-card card rounded-lg shadow p-4 p-xl-5 mb-4 text-center position-relative overflow-hidden">
+                            <div className="banner"></div>
+                            <img src={user1} alt="" className="img-circle mx-auto mb-3"/>
+                            <h3 className="mb-4">Kiran Acharya</h3>
+                            <div className="text-left mb-4">
+                                <p className="mb-2"><i class="fa fa-envelope mr-2"></i> kiranaa287@gmail.com</p>
+                                <p className="mb-2"><i class="fa fa-phone mr-2"></i> +91 9876543215</p>
+                                <p className="mb-2"><i class="fa fa-globe mr-2"></i> kiranwoe.com</p>
+                                <p className="mb-2"><i class="fa fa-map-marker-alt mr-2"></i> Bangalore</p>
+                            </div>
+                            <div className="social-links d-flex justify-content-center">
+                                <a href="#!" class="mx-2"><img src={dribble} alt="Dribbble"/></a>
+                                <a href="#!" class="mx-2"><img src={facebook} alt="Facebook"/></a>
+                                <a href="#!" class="mx-2"><img src={linkedin} alt="Linkedin"/></a>
+                                <a href="#!" class="mx-2"><img src={youtube} alt="Youtube"/> </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-8  col-md-6 col-lg-4 ">
+                        <div className="profile-card card rounded-lg shadow p-4 p-xl-5 mb-4 text-center position-relative overflow-hidden">
+                            <div className="banner"></div>
+                            <img src={user1} alt="" className="img-circle mx-auto mb-3"/>
+                            <h3 className="mb-4">Kiran Acharya</h3>
+                            <div className="text-left mb-4">
+                                <p className="mb-2"><i class="fa fa-envelope mr-2"></i> kiranaa287@gmail.com</p>
+                                <p className="mb-2"><i class="fa fa-phone mr-2"></i> +91 9876543215</p>
+                                <p className="mb-2"><i class="fa fa-globe mr-2"></i> kiranwoe.com</p>
+                                <p className="mb-2"><i class="fa fa-map-marker-alt mr-2"></i> Bangalore</p>
+                            </div>
+                            <div className="social-links d-flex justify-content-center">
+                                <a href="#!" class="mx-2"><img src={dribble} alt="Dribbble"/></a>
+                                <a href="#!" class="mx-2"><img src={facebook} alt="Facebook"/></a>
+                                <a href="#!" class="mx-2"><img src={linkedin} alt="Linkedin"/></a>
+                                <a href="#!" class="mx-2"><img src={youtube} alt="Youtube"/> </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-8 col-md-6 col-lg-4">
+                        <div className="profile-card card rounded-lg shadow p-4 p-xl-5 mb-4 text-center position-relative overflow-hidden">
+                            <div className="banner"></div>
+                            <img src={user1} alt="" className="img-circle mx-auto mb-3"/>
+                            <h3 className="mb-4">Kiran Acharya</h3>
+                            <div className="text-left mb-4">
+                                <p className="mb-2"><i class="fa fa-envelope mr-2"></i> kiranaa287@gmail.com</p>
+                                <p className="mb-2"><i class="fa fa-phone mr-2"></i> +91 9876543215</p>
+                                <p className="mb-2"><i class="fa fa-globe mr-2"></i> kiranwoe.com</p>
+                                <p className="mb-2"><i class="fa fa-map-marker-alt mr-2"></i> Bangalore</p>
+                            </div>
+                            <div className="social-links d-flex justify-content-center">
+                                <a href="#!" class="mx-2"><img src={dribble} alt="Dribbble"/></a>
+                                <a href="#!" class="mx-2"><img src={facebook} alt="Facebook"/></a>
+                                <a href="#!" class="mx-2"><img src={linkedin} alt="Linkedin"/></a>
+                                <a href="#!" class="mx-2"><img src={youtube} alt="Youtube"/> </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
 	    </section>
 	
