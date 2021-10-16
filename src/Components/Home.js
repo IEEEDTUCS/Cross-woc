76ec77f0f7a8e9bacf8018f6302ceb64fafc0f90
Jindalh2 <jindalh2@gmail.com>
2021-10-17 00:45:12 +0530

    Changes in home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Intro from './Intro';
 import Sponsor from './Sponsor';
 import Project from './Project';
-import './Home.css'
+import './Home.css';
+import {Jumbotron, Row, Card,Col,CardImg,CardBody,CardTitle,CardText, CardSubtitle, Button} from 'reactstrap';
+import Card1 from './CardUI';
 
 function Home() {
     return (
         <div class='Home'>
             <div className="Header text-center">
-                <h1 className='Title'>CrossWoc</h1>
+                <div className="container-fluid d-flex justify-content-center">
+                 <div className="row">
+                   <div className="col-md-4">
+                     <Card1/>
+                   </div>
+                   <div className="col-md-4">
+                      <Card1/>
+                     </div>
+                   <div className="col-md-4">
+                     <Card1/>
+                 </div>
+                  </div>
+                </div>
             </div>            
             <Intro />
             <Sponsor />
