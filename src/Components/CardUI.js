98bee82a5985b3fde4d995b372036cdf033ab0f0
Amal Chawla <chawla.amal.170401@gmail.com>
2021-10-20 10:34:00 +0530

    project card

---
+++ src/Components/CardUI.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/card-style.css';
+const Card = props=>{
+  return(
+   <div className="card text-center shadow">
+     <div className="overflow">
+       <img src={props.imgsrc} alt="" className="card-img-top"/>
+     </div>
+     <div className="card-body text-dark">
+      <h4 className="card-title">{props.title}</h4>
+      <p className="card-text text-secondary">
+       lorum ipusmkemek jenfef nfkbefjjb nfekfn kjbfejkfbejkfbejfbj bjbj bj bjb b jb j gyf yf yfffffffffyg guyg
+      </p> 
+      <a href="/" className="btn btn-outline-success">Go Any</a>
+   </div>
+   </div>
+
+  );
+}
+
+
+export default Card;
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import '../styles/Project.css';
+import Card1 from './CardUI';
+import img1 from '../img/img1.jpg';
+
 
 
 function Project() {
     return (
         <div className='Project'>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
-            <h1>Project Page</h1>
+            <div className="Header text-center">
+                <div className="container-fluid d-flex justify-content-center">
+                 <div className="row">
+                   <div className="col-md-4">
+                     <Card1 imgsrc={img1} title = "Card1"/>
+                   </div>
+                   <div className="col-md-4">
+                      <Card1 imgsrc = {img1} title = "Card2"/>
+                     </div>
+                   <div className="col-md-4">
+                   <Card1 imgsrc={img1} title = "Card3"/>
+                 </div>
+                  </div>
+                </div>
+            </div>
         </div>
     )
 }
